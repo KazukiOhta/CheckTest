eb34b32669ffa8cd0f76752166e4f01e52e57928
KazukiOhta <kazukiota121@gmail.com>
2019-04-13 16:01:48 +0900

    Level 3 Done.

--- Level1/ViewController.swift
+++ Level1/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var number = 0
+    var names = ["Apple", "Banana", "Carrot"]
+    var imagenames = ["apple.jpeg", "banana.jpeg", "carrot.jpg"]
+    @IBOutlet var label: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var btnA: UIButton!
+    @IBOutlet var btnB: UIButton!
+    @IBOutlet var btnC: UIButton!
+    var btns: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.label.layer.cornerRadius = 10
+        self.label.layer.masksToBounds = true
+        btns = [btnA, btnB, btnC]
     }
 
-    var number = 0
-    var names = ["Apple", "Banana", "Carrot"]
-    @IBOutlet var label: UILabel!
+    
     @IBAction func nextbtn(){
         number += 1
         number %= 3
@@ -26,6 +39,18 @@ class ViewController: UIViewController {
     @IBAction func prevbtn(){
         number += 2
         number %= 3
+        show()
+    }
+    @IBAction func nextswp(){
+        number += 1
+        number %= 3
+        print("nextswp")
+        show()
+    }
+    @IBAction func prevswp(){
+        number += 2
+        number %= 3
+        print("prevswp")
         show()
     }
     @IBAction func Abtn(){
@@ -42,6 +67,7 @@ class ViewController: UIViewController {
     }
     func show(){
         label.text = names[number]
+        imageView.image = UIImage(named: imagenames[number])
     }
 }
 
